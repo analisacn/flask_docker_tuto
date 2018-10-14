e9440fd197d9c6000578571dc04399d3e6fb4ea0
Analisa <13380980+analisacn@users.noreply.github.com>
2018-10-14 21:04:51 +0200

    Add API behaviour and some examples of endpoints using flask restplus. Add volume to docker-compose's web service

--- flask/app.py
+++ flask/app.py
@@ -1,63 +1,111 @@
-from flask import Flask
 import psycopg2
+from flask import Flask
+from flask_restplus import Api, Resource, abort, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
+post_user_parser = reqparse.RequestParser(bundle_errors=True)
+post_user_parser.add_argument('username', type=str, required=True)
+post_user_parser.add_argument('first_name', type=str, required=True)
+post_user_parser.add_argument('last_name', type=str, required=True)
 
 
-@app.route('/')
-def hello_world():
-    db_conn = connect()
-    cur = db_conn.cursor()
-
-    # Create a table
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS users ("
-        "id serial PRIMARY KEY, "
-        "username text NOT NULL UNIQUE, "
-        "first_name text NOT NULL, "
-        "last_name text NOT NULL);"
-    )
-    # Save changes to te DB
-    db_conn.commit()
-
-    # Add a row
-    add_user(cur, db_conn, 'analisac', 'Analisa', 'Carbone')
-
-    # Let's get some data
-    user = get_user_by_username(cur, "analisac")
-
-    # Close database connection
-    close_connection(db_conn, cur=cur)
-    msg = user and f'Hello {user[2]} {user[3]}' or "Hello Guest"
-    return msg
-
-
-def add_user(cur, conn, username, first_name='', last_name=''):
-    exists = get_user_by_username(cur, username)
-    if not exists:
-        cur.execute("INSERT INTO users (username, first_name, last_name) "
-                    "VALUES (%s, %s, %s)", (username, first_name, last_name)
-                    )
-        conn.commit()
-
-
-def get_user_by_username(cur, username):
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    user = cur.fetchone()  # (1, 'analisac', 'Analisa', 'Carbone')
-    return user
-
-
-def close_connection(connection, cur=False):
-    if cur:
-        cur.close()
+def close_connection(connection, cursor=False):
+    if cursor:
+        cursor.close()
     connection.close()
 
 
 def connect():
-
     c = psycopg2.connect(host='postgresql', port=5432, user='postgres',
-                          password='postgresexample', dbname='testdb')
+                         password='postgresexample', dbname='testdb')
     return c
+
+
+@api.route('/')
+class HelloWorld(Resource):
+    def get(self):
+        db_conn = connect()
+        cursor = db_conn.cursor()
+
+        # Create a table
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS users ("
+            "id serial PRIMARY KEY, "
+            "username text NOT NULL UNIQUE, "
+            "first_name text NOT NULL, "
+            "last_name text NOT NULL);"
+        )
+        db_conn.commit()
+        close_connection(db_conn, cursor=cursor)
+        msg = "Hello World"
+        return {'msg': msg}
+
+
+@api.route('/users')
+class Users(Resource):
+    def get(self):
+        """
+        Get all users list
+        """
+        db_conn = connect()
+        cursor = db_conn.cursor()
+        cursor.execute(f"SELECT * FROM users")
+        users = cursor.fetchall()
+        close_connection(db_conn, cursor=cursor)
+        return users
+
+    @api.expect(post_user_parser)
+    def post(self):
+        """
+        Add a new user
+        """
+        args = post_user_parser.parse_args()
+        username = args['username'].lower()
+
+        exists = User().get_user(username)
+
+        if exists:
+            abort(404, message=f"User {username} already exists")
+
+        db_conn = connect()
+        cursor = db_conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (username, first_name, last_name) "
+            "VALUES (%s, %s, %s)",
+            (username, args['first_name'], args['last_name'])
+        )
+        db_conn.commit()
+        close_connection(db_conn, cursor=cursor)
+        user_created = User().get_user(username)
+        return user_created, 201
+
+
+@api.route('/user/<username>')
+class User(Resource):
+    def get_user(self, username):
+        username = username.lower()
+        db_conn = connect()
+        cursor = db_conn.cursor()
+        cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
+        user = cursor.fetchone()  # (1, 'analisac', 'Analisa', 'Carbone')
+        close_connection(db_conn, cursor=cursor)
+        return user
+
+    def get(self, username):
+        user = self.get_user(username)
+        if not user:
+            abort(404, message=f"User {username} doesn't exist")
+
+        user_dict = {
+            'id': user[0],
+            'username': user[1],
+            'first_name': user[2],
+            'last_name': user[3]
+        }
+        return user_dict, 200
 
 
 if __name__ == '__main__':
